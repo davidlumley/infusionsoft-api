20f7f53e06f7c84c5b7913791792e6a64a55c965
David Lumley <david@davidlumley.com.au>
2013-11-07 09:51:10 +1000

    standardise contact data

--- lib/infusionsoft/api/models/contact_group.rb
+++ lib/infusionsoft/api/models/contact_group.rb
@@ -3,12 +3,23 @@ module Infusionsoft
     module Model
       class ContactGroup < Infusionsoft::Api::Model::Base
 
+        def all(query = {}, page_number = 0)
+          results = super(query, page_number)
+          results.map{|x|{
+            'FirstName'    => x['Contact.FirstName'],
+            'LastName'     => x['Contact.LastName'],
+            'Company'      => x['Contact.Company'],
+            'Email'        => x['Contact.Email'],
+            'ContactGroup' => x['ContactGroup'],
+          }}
+        end
+
         def table_name
           'ContactGroupAssign'
         end
 
         def fields
-          [:ContactGroup, :'Contact.FirstName', :'Contact.LastName', :'Contact.Company', :'Contact.Email']
+          [:'ContactGroup', :'Contact.FirstName', :'Contact.LastName', :'Contact.Company', :'Contact.Email']
         end
 
       end
