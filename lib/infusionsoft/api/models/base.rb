51712ba595cdf7bdb48c3d01e9edc69bb5f7a7bc
David Lumley <david@davidlumley.com.au>
2013-11-18 16:25:13 +1000

    add create and delete method for models

--- lib/infusionsoft/api/models/base.rb
+++ lib/infusionsoft/api/models/base.rb
@@ -13,6 +13,18 @@ module Infusionsoft
           results.length > 0 ? results + self.all(query, page_number + 1) : results
         end
 
+        def create(data = {})
+          @client.connection.call('DataService.add', @client.api_key, self.table_name, data)
+        end
+
+        def delete(query)
+          if query.is_a?(Hash)
+            self.all(query).each{|data| self.delete(data['Id'])}
+          else
+            @client.connection.call('DataService.delete', @client.api_key, self.table_name, query)
+          end
+        end
+
         def table_name
           @model_name
         end
