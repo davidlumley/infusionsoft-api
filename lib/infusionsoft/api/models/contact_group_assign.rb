c125eacbdcbe2f74721da706275c393125a9ec2c
David Lumley <david@davidlumley.com.au>
2013-11-20 16:20:55 +1000

    allow adding / removal of contact groups

--- lib/infusionsoft/api/models/contact_group_assign.rb
+++ lib/infusionsoft/api/models/contact_group_assign.rb
@@ -7,6 +7,7 @@ module Infusionsoft
           results = super(query, paginate, page_number)
           results.map{|x|{
             'Id'           => x['ContactId'],
+            'GroupId'      => x['GroupId'],
             'FirstName'    => x['Contact.FirstName'],
             'LastName'     => x['Contact.LastName'],
             'Company'      => x['Contact.Company'],
@@ -15,8 +16,37 @@ module Infusionsoft
           }}
         end
 
+        def create(data = {})
+          data = standardise_query(data)
+          @client.connection.call('ContactService.addToGroup', @client.api_key, data[:ContactId], data[:GroupId])
+        end
+
+        def delete(query = {})
+          query = standardise_query(query)
+          if query[:ContactId] && query[:GroupId]
+            @client.connection.call('ContactService.removeFromGroup', @client.api_key, query[:ContactId], query[:GroupId])
+          else
+            self.all(query).each do |data|
+              self.delete({
+                :ContactId => data['Id'],
+                :GroupId   => data['GroupId'],
+              })
+            end
+          end
+        end
+
         def fields
-          [:'ContactGroup', :'ContactId', :'Contact.FirstName', :'Contact.LastName', :'Contact.Company', :'Contact.Email']
+          [:'ContactGroup', :'ContactId', :'GroupId', :'Contact.FirstName', :'Contact.LastName', :'Contact.Company', :'Contact.Email']
+        end
+
+        private
+
+        def standardise_query(query = {})
+          if query['ContactId'] && query['GroupId']
+            query[:ContactId] = query['ContactId']
+            query[:GroupId]   = query['GroupId']
+          end
+          query
         end
 
       end
